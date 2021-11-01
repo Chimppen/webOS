52394140a73f1f4264b8a4d4ad75ef9bdea70811
Anas Mohamed <amohamed2479@gmail.com>
2021-11-01 13:46:51 -0500

    Create Screen component
    This component will act as the screen of the OS

---
+++ src/Screen.svelte
@@ -0,0 +1,4 @@
+<div bind:this="{$screen}" class="w-screen h-screen overflow-hidden bg-blue-50" style="background: url('./assets/background.jpg'); background-repeat:no-repeat; background-size:cover; background-position:top;">
+</div>
+<script>
+</script>
