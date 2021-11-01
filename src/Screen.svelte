5b326da9cd47b2e9d35d44f2afd9768c658c7523
Anas Mohamed <amohamed2479@gmail.com>
2021-11-01 13:57:38 -0500

    Add Topbar to the screen

--- src/Screen.svelte
+++ src/Screen.svelte
@@ -1,4 +1,5 @@
 <div bind:this="{$screen}" class="w-screen h-screen overflow-hidden bg-blue-50" style="background: url('./assets/background.jpg'); background-repeat:no-repeat; background-size:cover; background-position:top;">
+    <Topbar/>
 
     <div class="fixed bottom-0 w-full mb-2">
         <Taskbar on:openApp={openApp}/>
@@ -7,4 +8,7 @@
 
 <script>
 import Taskbar from "./components/Taskbar.svelte";
+
+import Topbar from "./components/Topbar.svelte";
+
 </script>
