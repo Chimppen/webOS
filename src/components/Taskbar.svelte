139e6e8ee99f1730876dc16ed53b6737db359fc2
Anas Mohamed <amohamed2479@gmail.com>
2021-11-01 13:51:10 -0500

    Create Taskbar Component

---
+++ src/components/Taskbar.svelte
@@ -0,0 +1,13 @@
+<div bind:this="{taskbar}" class="max-w-[800px] mx-auto p-4 rounded-md backdrop-filter backdrop-blur-md flex justify-start gap-3">
+
+</div>
+
+<script>
+
+
+let taskbar
+
+
+
+
+</script>
