ae41fa1032bd3f5f4743d3e1dbffa97b20590a7a
Anas Mohamed <amohamed2479@gmail.com>
2021-11-01 13:52:20 -0500

    add the Taskbar component to the Screen

--- src/Screen.svelte
+++ src/Screen.svelte
@@ -1,4 +1,10 @@
 <div bind:this="{$screen}" class="w-screen h-screen overflow-hidden bg-blue-50" style="background: url('./assets/background.jpg'); background-repeat:no-repeat; background-size:cover; background-position:top;">
+
+    <div class="fixed bottom-0 w-full mb-2">
+        <Taskbar on:openApp={openApp}/>
+    </div>
 </div>
+
 <script>
+import Taskbar from "./components/Taskbar.svelte";
 </script>
--- src/components/Taskbar.svelte
+++ src/components/Taskbar.svelte
@@ -1,12 +1,32 @@
 <div bind:this="{taskbar}" class="max-w-[800px] mx-auto p-4 rounded-md backdrop-filter backdrop-blur-md flex justify-start gap-3">
 
+    {#each $apps as app}
+        <button on:click="{newApp(app)}" class=""> 
+            <AppIcon name={app.name} icon={app.icon}/>
+        </button>
+    {/each}
+
 </div>
 
 <script>
+import { apps } from "../memory/apps";
+import { screen } from "../memory/screen";
+import { createEventDispatcher } from "svelte";
+import AppIcon from "./app-icon.svelte";
+import Topbar from "./Topbar.svelte";
 
+let dispatch = createEventDispatcher()
 
 let taskbar
 
+
+function newApp(app){
+    
+    dispatch('openApp',{
+        app: app.main()
+    })
+
+}
 
 
 
