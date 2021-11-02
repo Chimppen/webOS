89ce4cd21dbcd8cea6993911f7bb72e0261db786
Anas Mohamed <amohamed2479@gmail.com>
2021-11-02 14:37:34 -0500

    Create app icon component
    This is the component that'll display app icons

---
+++ src/components/app-icon.svelte
@@ -0,0 +1,17 @@
+<div bind:this="{iconContainer}" class="p-2 rounded-2xl bg-white">
+
+</div>
+
+<script>
+import { onMount } from "svelte";
+
+let iconContainer
+
+export let icon
+
+onMount(()=>{
+
+    iconContainer.innerHTML = icon
+})
+
+</script>
