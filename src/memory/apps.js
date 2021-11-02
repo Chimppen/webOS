c2e8a5c2ffec5399bd3078b4bc3e68e8bd7e1891
Anas Mohamed <amohamed2479@gmail.com>
2021-11-02 14:41:46 -0500

    register browser app into the OS

--- src/memory/apps.js
+++ src/memory/apps.js
@@ -1,7 +1,13 @@
 import { writable } from "svelte/store";
+import * as browser from '../../apps/browser/app.config'
 
 
 export let apps = writable([
+           {
+               name: browser.appName,
+               icon:browser.appIcon,
+               main:browser.main
+           }
 ])
 
     
